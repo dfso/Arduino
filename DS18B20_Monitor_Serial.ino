f548c1ddba3c7d929c6ffb5c4b5fbbde0ca1731b
dfso <denison.new@gmail.com>
2017-06-12 22:55:40 -0400

    Add files via upload

---
+++ DS18B20_Monitor_Serial.ino
@@ -0,0 +1,46 @@
+/*
+ * Pinagem   
+ * -> Sensor DS18B20
+ *      pino central (dados) - Pin 7
+ */
+
+//*****************************
+// bibliotecas do DS18B20
+#include <OneWire.h>
+#include <DallasTemperature.h>
+//*****************************
+
+#include <OLED_I2C.h>
+
+//***********************************************************************************
+// porta digital do arduino em que o pino de dados do sensor DS18B20 está ligado
+#define ONE_WIRE_BUS 7
+//***********************************************************************************
+
+// Prepara uma instancia de oneWire para se comunicar com um dispositivo OneWire
+OneWire oneWire(ONE_WIRE_BUS);
+// Passa nossa referencia oneWire para Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+//************************************************************************************
+
+
+// pra calcular a temp.
+float tempC = 0.0;
+
+
+void setup() {
+    sensors.begin();
+    Serial.begin(9600);
+}
+
+
+void loop() {
+
+    // envia um comando para obter a temperatura
+    sensors.requestTemperatures();
+    tempC = sensors.getTempCByIndex(0);
+
+    Serial.println(tempC);
+
+    delay(1000);
+}
---
+++ semaforo.ino
@@ -0,0 +1,57 @@
+#define VD1 8
+#define AM1 9
+#define VM1 10
+
+#define VD2 5
+#define AM2 6
+#define VM2 7
+
+void setup() {
+
+    pinMode(VD1, OUTPUT);
+    pinMode(AM1, OUTPUT);
+    pinMode(VM1, OUTPUT);
+
+    pinMode(VD2, OUTPUT);
+    pinMode(AM2, OUTPUT);
+    pinMode(VM2, OUTPUT);
+}
+
+void loop() {
+
+    digitalWrite(VD1, HIGH);
+    digitalWrite(VM2, HIGH);
+    delay(7000);
+    digitalWrite(VD1, LOW);
+    piscarVerde(VD1, 3);
+    
+    digitalWrite(AM1, HIGH);
+    delay(5000);
+    digitalWrite(AM1, LOW);
+    
+    digitalWrite(VM1, HIGH);
+    digitalWrite(VM2, LOW);
+    
+    digitalWrite(VD2, HIGH);
+    delay(7000);
+    digitalWrite(VD2, LOW);
+    piscarVerde(VD2, 3);
+    
+    digitalWrite(AM2, HIGH);
+    delay(5000);
+    digitalWrite(AM2, LOW);
+    digitalWrite(VM1, LOW);
+}
+
+// faz o led piscar durante o intervalo passado como parâmetro
+void piscarVerde(int pin, int seconds) {
+
+    for(int i=0; i<seconds; i++) {
+        digitalWrite(pin, HIGH);
+        delay(500);
+        digitalWrite(pin, LOW);
+        delay(500);
+    }
+        
+}
+
